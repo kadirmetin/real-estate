827092c8eb5b8f0aac7cff832fb99dc4d4eff067
Kadir Metin <kadir-metin@outlook.com.tr>
2024-04-01 16:13:04 +0300

    Prisma added

--- api/app.js
+++ api/app.js
@@ -1,9 +1,18 @@
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import express from "express";
+import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
+
+dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8080, () => {
   console.log("Server is running!");
